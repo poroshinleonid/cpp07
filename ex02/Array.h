b731872bce7f8b56b8b16903216a071c85361251
Leonid Poroshin <lporoshi@student.42heilbronn.de>
2024-04-29 21:38:01 +0200

    ex02: finish Array implementation

--- ex02/Array.h
+++ ex02/Array.h
@@ -1,18 +1,60 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
+#include <exception>
+
 template <typename T> class Array {
 public:
-  Array();
-  Array(unsigned int n);
-  Array(const Array &other);
-  Array &operator=(const Array &other);
+  Array() : size_(0), storage_(new T[0]) {}
+  Array(unsigned int n) : size_(n), storage_(new T[n]) {
+    for (unsigned int i = 0; i < size_; i++) {
+      storage_[i] = T();
+    }
+  }
+  Array(const Array &other) : size_(other.size_) {
+    storage_ = new T[size_];
+    for (unsigned int i = 0; i < size_; i++) {
+      storage_[i] = other.storage_[i];
+    }
+  }
 
-  const unsigned int &size() const;
+  Array &operator=(const Array &other) {
+    if (this != &other) {
+      size_ = other.size_;
+      delete[] storage_;
+      storage_ = new T[size_];
+      for (unsigned int i = 0; i < size_; i++) {
+        storage_[i] = other.storage_[i];
+      }
+    }
+    return *this;
+  }
+  ~Array() {
+    if (storage_) {
+      delete[] storage_;
+    }
+  }
+
+  class OutOfRangeException : public std::exception {
+    virtual const char* what() const throw() {
+      return "Index out of range";
+    }
+  };
+
+  T &operator[](const unsigned int index) {
+    if (index >= size_) {
+      throw OutOfRangeException();
+    }
+    return storage_[index];
+  }
+
+  const unsigned int &size() const {
+    return size_;
+  }
 
 private:
-  int size_;
-  T *storage;
+  unsigned int size_;
+  T *storage_;
 };
 
 #endif
